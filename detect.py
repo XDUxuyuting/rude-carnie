233880cf7aa87a23e726a824751ebf609d243ebd
XDUxuyuting <51498360+XDUxuyuting@users.noreply.github.com>
2019-10-13 14:07:23 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -19,27 +19,27 @@ class ObjectDetectorCascadeOpenCV(ObjectDetector):
         self.min_width_thresh = min_width_thresh
         self.tgtdir = tgtdir
         self.basename = basename
-        self.face_cascade = cv2.CascadeClassifier(model_name)
+        self.face_cascade = cv2.CascadeClassifier(model_name) #调用opencv级联分类器
 
     def run(self, image_file):
         print(image_file)
         img = cv2.imread(image_file)
         min_h = int(max(img.shape[0] / self.min_height_dec, self.min_height_thresh))
         min_w = int(max(img.shape[1] / self.min_width_dec, self.min_width_thresh))
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #转化灰度图像
         faces = self.face_cascade.detectMultiScale(gray, 1.3, minNeighbors=5, minSize=(min_h, min_w))
 
         images = []
-        for i, (x, y, w, h) in enumerate(faces):
-            images.append(self.sub_image('%s/%s-%d.jpg' % (self.tgtdir, self.basename, i + 1), img, x, y, w, h))
+        for i, (x, y, w, h) in enumerate(faces): #枚举
+            images.append(self.sub_image('%s/%s-%d.jpg' % (self.tgtdir, self.basename, i + 1), img, x, y, w, h)) #增补数据
 
         print('%d faces detected' % len(images))
 
         for (x, y, w, h) in faces:
-            self.draw_rect(img, x, y, w, h)
+            self.draw_rect(img, x, y, w, h) #绘制
             # Fix in case nothing found in the image
         outfile = '%s/%s.jpg' % (self.tgtdir, self.basename)
-        cv2.imwrite(outfile, img)
+        cv2.imwrite(outfile, img)#保存图像为指定文件名
         return images, outfile
 
     def sub_image(self, name, img, x, y, w, h):
