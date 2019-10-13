52b0484762ae4c15eca0793f72555b717bade721
XDUxuyuting <51498360+XDUxuyuting@users.noreply.github.com>
2019-10-13 14:19:56 +0800

    Update dlibdetect.py

--- dlibdetect.py
+++ dlibdetect.py
@@ -4,12 +4,12 @@ import dlib
 import cv2
 FACE_PAD = 50
 
-class FaceDetectorDlib(ObjectDetector):
+class FaceDetectorDlib(ObjectDetector): #人脸识别数据库
     def __init__(self, model_name, basename='frontal-face', tgtdir='.'):
         self.tgtdir = tgtdir
         self.basename = basename
-        self.detector = dlib.get_frontal_face_detector()
-        self.predictor = dlib.shape_predictor(model_name)
+        self.detector = dlib.get_frontal_face_detector() #进行人脸检测，提取人脸外部矩形框
+        self.predictor = dlib.shape_predictor(model_name) #人脸面部轮廓特征提取
 
     def run(self, image_file):
         print(image_file)
@@ -18,7 +18,7 @@ class FaceDetectorDlib(ObjectDetector):
         faces = self.detector(gray, 1)
         images = []
         bb = []
-        for (i, rect) in enumerate(faces):
+        for (i, rect) in enumerate(faces): 
             x = rect.left()
             y = rect.top()
             w = rect.right() - x
@@ -26,14 +26,14 @@ class FaceDetectorDlib(ObjectDetector):
             bb.append((x,y,w,h))
             images.append(self.sub_image('%s/%s-%d.jpg' % (self.tgtdir, self.basename, i + 1), img, x, y, w, h))
 
-        print('%d faces detected' % len(images))
+        print('%d faces detected' % len(images)) #输出检测的人脸数
 
         for (x, y, w, h) in bb:
             self.draw_rect(img, x, y, w, h)
             # Fix in case nothing found in the image
         outfile = '%s/%s.jpg' % (self.tgtdir, self.basename)
         cv2.imwrite(outfile, img)
-        return images, outfile
+        return images, outfile #保存且返回图片和保存文件
 
     def sub_image(self, name, img, x, y, w, h):
         upper_cut = [min(img.shape[0], y + h + FACE_PAD), min(img.shape[1], x + w + FACE_PAD)]
@@ -45,4 +45,4 @@ class FaceDetectorDlib(ObjectDetector):
     def draw_rect(self, img, x, y, w, h):
         upper_cut = [min(img.shape[0], y + h + FACE_PAD), min(img.shape[1], x + w + FACE_PAD)]
         lower_cut = [max(y - FACE_PAD, 0), max(x - FACE_PAD, 0)]
-        cv2.rectangle(img, (lower_cut[1], lower_cut[0]), (upper_cut[1], upper_cut[0]), (255, 0, 0), 2)
+        cv2.rectangle(img, (lower_cut[1], lower_cut[0]), (upper_cut[1], upper_cut[0]), (255, 0, 0), 2) #在图像上画矩形
